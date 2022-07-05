5cb44ced18e2206f69d8293a9a0da4b07504fd07
Jtawfik04 <94028535+Jtawfik04@users.noreply.github.com>
2022-07-05 13:51:59 -0400

    Create MarketMake.js

---
+++ packages/hardhat/scripts/MarketMake.js
@@ -0,0 +1,26 @@
+const cron = require("node-cron");
+const MonitorEth = require("./Monitor");
+
+const httpProvider =
+  "MY INFURA ROPSTEN KEY";
+const to = "MY METAMASK ADDRESS";
+
+async function marketMake() {
+  try {
+    const monitor = new MonitorEth(httpProvider);
+    await monitor.initializeLastSyncedBlock();
+    console.log('Looking for transactions...')
+
+    cron.schedule("*/2 * * * *", async () => {
+      console.log('Cron started.')
+      await monitor.searchTransaction(to);
+      console.log('Cron finished.')
+      
+      //Transaction found, now marketmaking assuming token is minted
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+marketMake();
